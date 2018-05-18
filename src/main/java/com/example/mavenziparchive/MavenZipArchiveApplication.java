6837f18f04b2bb35ef4cc3d4b53200395b404ed5
neo <neoip.0514@gmail.com>
2018-05-19 08:23:22 +0900

    1st commit

---
+++ src/main/java/com/example/mavenziparchive/MavenZipArchiveApplication.java
@@ -0,0 +1,12 @@
+package com.example.mavenziparchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenZipArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenZipArchiveApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/mavenziparchive/MavenZipArchiveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.mavenziparchive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MavenZipArchiveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
